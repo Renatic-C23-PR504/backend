fb83bcda92600684c73bfd0167f6200e140644ab
Zhyraa <roralolxd@gmail.com>
2023-05-23 01:34:38 +0700

    some fixes
    trigger start, port 8080, simple get

--- routes/paths.js
+++ routes/paths.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const router = express.Router();
 const Multer = require('multer');
-const imgUpload = require('../modules/imgUpload');
+// const imgUpload = require('../modules/imgUpload');
 
 const multer = Multer({
    storage: Multer.MemoryStorage,
@@ -17,14 +17,29 @@ const connection = mysql.createConnection({
    password: 'password_sql_Anda',
 });
 
-router.get('/dashboard', (req, res) => {
-   const query =
-      'select (select count(*) from records where month(records.date) = month(now()) AND year(records.date) = year(now())) as month_records, (select sum(amount) from records) as total_amount;';
+if (!connection) {
+   console.log('gagal');
+}
+
+router.get('/all', (req, res) => {
+   const query = 'select * from user';
    connection.query(query, (err, rows, field) => {
       if (err) {
-         res.status(500).send({ message: err.sqlMessage });
+         res.status(200).send({ message: err.sqlMessage });
       } else {
          res.json(rows);
       }
    });
 });
+
+// router.get('/dashboard', (req, res) => {
+//    const query =
+//       'select (select count(*) from records where month(records.date) = month(now()) AND year(records.date) = year(now())) as month_records, (select sum(amount) from records) as total_amount;';
+//    connection.query(query, (err, rows, field) => {
+//       if (err) {
+//          res.status(500).send({ message: err.sqlMessage });
+//       } else {
+//          res.json(rows);
+//       }
+//    });
+// });

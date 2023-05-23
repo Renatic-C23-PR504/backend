975daa92a4125e7a506ab53794667626d606cdff
Zhyraa <roralolxd@gmail.com>
2023-05-23 14:41:29 +0700

    testing again

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require('express');
 const router = require('./routes/paths');
 const app = express();
 const port = 8080;
-const bodyParser = require('body-parser');
-
-const db = require('./routes/paths');
-
-//nangkep form jadi json
-app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
    res.send('Hello World!');
@@ -18,12 +12,3 @@ app.use(router);
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`);
 });
-
-//ngambil data 
-app.get('/data', (req, res) => {
-   db.query("SELECT * FROM users", (error, result) => {
-      //hasil mysql
-      console.log(result)
-      res.send(result)
-   })
-})
--- routes/paths.js
+++ routes/paths.js
@@ -1,27 +1,21 @@
-// const express = require('express');
+const express = require('express');
 const mysql = require('mysql');
-// const router = express.Router();
-// // const Multer = require('multer');
-// const imgUpload = require('../modules/imgUpload');
-
-// const multer = Multer({
-//    storage: Multer.MemoryStorage,
-//    fileSize: 5 * 1024 * 1024,
-// });
+const router = express.Router();
 
 // TODO: Sesuaikan konfigurasi database
 const connection = mysql.createConnection({
    host: '34.101.152.116',
    user: 'root',
-   database: 'nama_database_Anda',
-   password: 'password_sql_Anda',
+   database: 'main_db',
+   password: '/dR/%prDH0I5r)F>',
 });
 
-// if (!connection) {
-//    console.log('gagal');
-// }
+if (!connection) {
+   console.log('gagal');
+}
 
 router.get('/all', (req, res) => {
+   console.log(req);
    const query = 'select * from user';
    connection.query(query, (err, rows, field) => {
       if (err) {
@@ -44,5 +38,3 @@ module.exports = router;
 //       }
 //    });
 // });
-
-module.exports = connection

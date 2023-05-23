c57e34fe2012af2fced2631cd101b21e765a80aa
Zhyraa <roralolxd@gmail.com>
2023-05-23 14:45:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const router = require('./routes/paths');
 const app = express();
 const port = 8080;
 
+// const bodyParser = require('body-parser');
+// nangkep form jadi json
+// app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
    res.send('Hello World!');
 });

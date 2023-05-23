5ba2019159b96dfd269b015109a0e7c1abe2f03b
Barita <simpanaja12@gmail.com>
2023-05-23 13:31:51 +0700

    .

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const bodyParser = require('body-parser');
+
+const db = require('./routes/paths');
+
+//nangkep form jadi json
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
    res.send('Hello World!');
 });
 
+app.get('/cek', (req, res) => {
+   res.send('ini cek!');
+});
+
+//ngecek request dari form
+app.put('/form1', (req, res) => {
+   console.log({ testRequest: req.body })
+   res.send('berhasil request')
+})
+
+app.get('/form2', (req, res) => {
+   console.log({ urlParam: req.query.umur })
+   res.send('berhasil request umur aja')
+})
+
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`);
 });
+
+//ngambil data 
+app.get('/data', (req, res) => {
+   db.query("SELECT * FROM users", (error, result) => {
+      //hasil mysql
+      console.log(result)
+      res.send(result)
+   })
+})
--- routes/paths.js
+++ routes/paths.js
@@ -1,36 +1,37 @@
-const express = require('express');
+// const express = require('express');
 const mysql = require('mysql');
-const router = express.Router();
-const Multer = require('multer');
+// const router = express.Router();
+// const Multer = require('multer');
 // const imgUpload = require('../modules/imgUpload');
 
-const multer = Multer({
-   storage: Multer.MemoryStorage,
-   fileSize: 5 * 1024 * 1024,
-});
+// const multer = Multer({
+//    storage: Multer.MemoryStorage,
+//    fileSize: 5 * 1024 * 1024,
+// });
 
 // TODO: Sesuaikan konfigurasi database
 const connection = mysql.createConnection({
-   host: 'public_ip_sql_instance_Anda',
+   host: '34.101.152.116',
    user: 'root',
-   database: 'nama_database_Anda',
-   password: 'password_sql_Anda',
+   database: 'renatic-db',
+   port: '8080',
+   password: '/dR/%prDH0I5r)F>',
 });
 
-if (!connection) {
-   console.log('gagal');
-}
+// if (!connection) {
+//    console.log('gagal');
+// }
 
-router.get('/all', (req, res) => {
-   const query = 'select * from user';
-   connection.query(query, (err, rows, field) => {
-      if (err) {
-         res.status(200).send({ message: err.sqlMessage });
-      } else {
-         res.json(rows);
-      }
-   });
-});
+// router.get('/all', (req, res) => {
+//    const query = 'select * from user';
+//    connection.query(query, (err, rows, field) => {
+//       if (err) {
+//          res.status(200).send({ message: err.sqlMessage });
+//       } else {
+//          res.json(rows);
+//       }
+//    });
+// });
 
 // router.get('/dashboard', (req, res) => {
 //    const query =
@@ -43,3 +44,5 @@ router.get('/all', (req, res) => {
 //       }
 //    });
 // });
+
+module.exports = connection

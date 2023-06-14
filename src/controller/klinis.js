3f7f37f882b654a9fb4f548416b996282442bee5
Zhyraa <roralolxd@gmail.com>
2023-06-15 00:19:25 +0700

    Update klinis.js

--- src/controller/klinis.js
+++ src/controller/klinis.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
+const axios = require('axios');
 const app = express.Router();
 const connection = require('../database');
 
@@ -27,8 +27,15 @@ const allKlinis = (req, res) => {
 
 const addKlinis = (req, res) => {
    let id = req.headers.id;
-   const { pregnancies, glucose, blood, skin, insulin, bmi, diabetes } =
-      req.body;
+   const {
+      Pregnancies,
+      Glucose,
+      BloodPressure,
+      SkinThickness,
+      Insulin,
+      BMI,
+      DiabetesPedigreeFunction,
+   } = req.body;
 
    if (!id) {
       return res.status(417).json({
@@ -38,13 +45,13 @@ const addKlinis = (req, res) => {
    }
 
    const allowedKeys = [
-      'pregnancies',
-      'glucose',
-      'blood',
-      'skin',
-      'insulin',
-      'bmi',
-      'diabetes',
+      'Pregnancies',
+      'Glucose',
+      'BloodPressure',
+      'SkinThickness',
+      'Insulin',
+      'BMI',
+      'DiabetesPedigreeFunction',
    ];
    const receivedKeys = Object.keys(req.body);
 
@@ -58,24 +65,92 @@ const addKlinis = (req, res) => {
       });
    }
    if (
-      !pregnancies ||
-      !glucose ||
-      !blood ||
-      !skin ||
-      !insulin ||
-      !bmi ||
-      !diabetes
+      !Pregnancies ||
+      !Glucose ||
+      !BloodPressure ||
+      !SkinThickness ||
+      !Insulin ||
+      !BMI ||
+      !DiabetesPedigreeFunction
    ) {
       return res.status(417).json({
          error: 'true',
          message: 'data tidak boleh ada yang kosong',
       });
    }
+   const getAge = `SELECT k.*, p.tanggalLahir FROM klinis k JOIN patients p ON k.patient = p.idPatient WHERE idPatient = ?`;
+   connection.query(getAge, [id], (err, rows) => {
+      if (err) {
+         res.status(500).send({
+            error: 'true',
+            message: 'Terjadi kesalahan pada server',
+            err,
+         });
+      }
+
+      if (rows.length === 0) {
+         return res.status(417).json({
+            error: 'true',
+            message: 'pasien tidak ditemukan',
+         });
+      }
+
+      // Retrieve the birthdate from the database rows
+      const birthDate = new Date(rows[0].tanggalLahir);
+
+      // Calculate the age based on the current date
+      const currentDate = new Date();
+      const ageInMilliseconds = currentDate - birthDate;
+      const ageInYears = Math.floor(
+         ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25)
+      );
+
+      console.log('Age:', ageInYears);
+
+      axios
+         .post('https://clinic-nhldbkcx5q-et.a.run.app/', {
+            Pregnancies,
+            Glucose,
+            BloodPressure,
+            SkinThickness,
+            Insulin,
+            BMI,
+            DiabetesPedigreeFunction,
+            ageInYears,
+         })
+         .then((response) => {
+            // Extract the result from the other API response
+            const result = response.data;
+            console.log(result);
+            // Create a new API response based on the result
+            // const transformedData =
+            //    /* Process the result here */
+
+            //    // Send the new API response
+            //    res.json(transformedData);
+         })
+         .catch((error) => {
+            // Handle errors
+            return res.status(500).json({
+               error: 'true',
+               message: 'API tidak ditemukan',
+            });
+         });
+   });
 
    addDataKlinis = `INSERT INTO klinis(patient, pregnancies, glucose, blood, skin, insulin, bmi, diabetesDegree) VALUES (?,?,?,?,?,?,?,?)`;
    connection.query(
       addDataKlinis,
-      [id, pregnancies, glucose, blood, skin, insulin, bmi, diabetes],
+      [
+         id,
+         Pregnancies,
+         Glucose,
+         BloodPressure,
+         SkinThickness,
+         Insulin,
+         BMI,
+         DiabetesPedigreeFunction,
+      ],
       (err, rows) => {
          if (err) {
             res.status(500).send({
